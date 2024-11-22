db25ee57e641ed03cd476979f030ed2f6f402594
Alvapoor <alvgarmar10@alu.edu.gva.es>
2024-11-22 09:51:27 +0100

    workspace

---
+++ ejercicios funciones/ejercicios4Funciones.py
@@ -0,0 +1,1 @@
+#Diseñar una función que calcule el área y el perímetro de una circunferencia. Utiliza dicha función en un programa principal que lea el radio de una circunferencia y muestre su área y perímetro.
